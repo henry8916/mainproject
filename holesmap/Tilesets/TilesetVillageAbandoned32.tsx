0e2971f10c1734c8d1975fd7c689a2484234aa83
henry8916 <165128675+henry8916@users.noreply.github.com>
2024-11-21 01:50:51 +0900

    Add files via upload

---
+++ holesmap/Tilesets/TilesetVillageAbandoned32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="TilesetVillageAbandoned32" tilewidth="32" tileheight="32" tilecount="240" columns="20">
+ <image source="TilesetVillageAbandoned32.png" width="640" height="384"/>
+</tileset>
