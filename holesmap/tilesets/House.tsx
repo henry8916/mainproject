04d11dc19059866497c570664a65ebaa48416d64
yul080708 <yul080708@gmail.com>
2024-11-28 05:37:48 +0900

    Add files via upload

---
+++ holesmap/tilesets/House.tsx
@@ -0,0 +1,145 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="House" tilewidth="1056" tileheight="834" tilecount="47" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../Tilesets/Campfire.png" width="320" height="322"/>
+ </tile>
+ <tile id="2">
+  <image source="../Tilesets/Nametag.png" width="127" height="38"/>
+ </tile>
+ <tile id="3">
+  <image source="../Tilesets/Rock1.png" width="60" height="34"/>
+ </tile>
+ <tile id="4">
+  <image source="../Tilesets/Rock2.png" width="65" height="55"/>
+ </tile>
+ <tile id="5">
+  <image source="../Tilesets/Rock3.png" width="55" height="34"/>
+ </tile>
+ <tile id="6">
+  <image source="../Tilesets/Rock4.png" width="65" height="55"/>
+ </tile>
+ <tile id="7">
+  <image source="../Tilesets/Rock5.png" width="127" height="92"/>
+ </tile>
+ <tile id="8">
+  <image source="../Tilesets/Rock6.png" width="66" height="97"/>
+ </tile>
+ <tile id="9">
+  <image source="../Tilesets/Rock7.png" width="127" height="93"/>
+ </tile>
+ <tile id="10">
+  <image source="../Tilesets/Rock8.png" width="64" height="99"/>
+ </tile>
+ <tile id="11">
+  <image source="../Tilesets/TilesetDesert32.png" width="640" height="384"/>
+ </tile>
+ <tile id="12">
+  <image source="../Tilesets/TilesetElement32.png" width="512" height="480"/>
+ </tile>
+ <tile id="13">
+  <image source="../Tilesets/TilesetFloor32.png" width="704" height="834"/>
+ </tile>
+ <tile id="14">
+  <image source="../Tilesets/TilesetFloorDetail32.png" width="512" height="160"/>
+ </tile>
+ <tile id="15">
+  <image source="../Tilesets/TilesetHole32.png" width="352" height="160"/>
+ </tile>
+ <tile id="16">
+  <image source="../Tilesets/TilesetHouse1.png" width="127" height="94"/>
+ </tile>
+ <tile id="17">
+  <image source="../Tilesets/TilesetHouse2.png" width="126" height="96"/>
+ </tile>
+ <tile id="18">
+  <image source="../Tilesets/TilesetHouse3.png" width="94" height="81"/>
+ </tile>
+ <tile id="19">
+  <image source="../Tilesets/TilesetHouse4.png" width="94" height="164"/>
+ </tile>
+ <tile id="20">
+  <image source="../Tilesets/TilesetHouse5.png" width="95" height="99"/>
+ </tile>
+ <tile id="21">
+  <image source="../Tilesets/TilesetHouse6.png" width="100" height="74"/>
+ </tile>
+ <tile id="22">
+  <image source="../Tilesets/TilesetHouse7.png" width="126" height="158"/>
+ </tile>
+ <tile id="23">
+  <image source="../Tilesets/TilesetHouse8.png" width="405" height="303"/>
+ </tile>
+ <tile id="24">
+  <image source="../Tilesets/TilesetHouse9.tsx.png" width="289" height="300"/>
+ </tile>
+ <tile id="25">
+  <image source="../Tilesets/TilesetHouse10.png" width="192" height="286"/>
+ </tile>
+ <tile id="26">
+  <image source="../Tilesets/TIlesetHouse11.png" width="222" height="244"/>
+ </tile>
+ <tile id="27">
+  <image source="../Tilesets/TIlesetHouse12.png" width="305" height="240"/>
+ </tile>
+ <tile id="28">
+  <image source="../Tilesets/TilesetHouse13.png" width="192" height="244"/>
+ </tile>
+ <tile id="29">
+  <image source="../Tilesets/TilesetHouse14.png" width="192" height="273"/>
+ </tile>
+ <tile id="30">
+  <image source="../Tilesets/TIlesetHouse15.png" width="195" height="276"/>
+ </tile>
+ <tile id="31">
+  <image source="../Tilesets/TilesetHouse32.png" width="1056" height="736"/>
+ </tile>
+ <tile id="32">
+  <image source="../Tilesets/TilesetLogic32.png" width="256" height="320"/>
+ </tile>
+ <tile id="33">
+  <image source="../Tilesets/TilesetNature32.png" width="768" height="672"/>
+ </tile>
+ <tile id="34">
+  <image source="../Tilesets/TilesetRelief.png" width="320" height="192"/>
+ </tile>
+ <tile id="35">
+  <image source="../Tilesets/TilesetReliefDetail.png" width="192" height="192"/>
+ </tile>
+ <tile id="36">
+  <image source="../Tilesets/TilesetTowers.png" width="384" height="96"/>
+ </tile>
+ <tile id="37">
+  <image source="../Tilesets/TilesetVillageAbandoned32.png" width="640" height="384"/>
+ </tile>
+ <tile id="38">
+  <image source="../Tilesets/TilesetWater32.png" width="896" height="544"/>
+ </tile>
+ <tile id="39">
+  <image source="../Tilesets/Tree1.png" width="61" height="45"/>
+ </tile>
+ <tile id="40">
+  <image source="../Tilesets/Tree2.png" width="90" height="98"/>
+ </tile>
+ <tile id="41">
+  <image source="../Tilesets/Tree3.png" width="87" height="87"/>
+ </tile>
+ <tile id="42">
+  <image source="../Tilesets/Tree5.png" width="75" height="82"/>
+ </tile>
+ <tile id="43">
+  <image source="../Tilesets/Tree6.png" width="33" height="66"/>
+ </tile>
+ <tile id="44">
+  <image source="../Tilesets/Tree7.png" width="33" height="29"/>
+ </tile>
+ <tile id="45">
+  <image source="../Tilesets/Tree8.png" width="31" height="58"/>
+ </tile>
+ <tile id="46">
+  <image source="../Tilesets/Tree9.png" width="32" height="35"/>
+ </tile>
+ <tile id="47">
+  <image source="../Tilesets/Tree10.png" width="31" height="26"/>
+ </tile>
+</tileset>
---
+++ holesmap/tilesets/TilesetDesert32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="TilesetDesert32" tilewidth="32" tileheight="32" tilecount="240" columns="20">
+ <image source="TilesetDesert32.png" width="640" height="384"/>
+</tileset>
