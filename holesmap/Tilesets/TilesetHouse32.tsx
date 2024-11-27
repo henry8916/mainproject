54bf488e8d9847eaf92caac455d99e4769f28c7a
yul080708 <yul080708@gmail.com>
2024-11-28 06:36:34 +0900

    Add files via upload

---
+++ holesmap/Tilesets/TilesetHouse32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="TilesetHouse32" tilewidth="32" tileheight="32" tilecount="759" columns="33">
+ <image source="TilesetHouse32.png" width="1056" height="736"/>
+</tileset>
---
+++ holesmap/Tilesets/TilesetWater32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="TilesetWater32" tilewidth="32" tileheight="32" tilecount="476" columns="28">
+ <image source="TilesetWater32.png" width="896" height="544"/>
+</tileset>
