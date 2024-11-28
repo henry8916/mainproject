83f35cb2e7664ad6804b681290a31662c759599c
henry8916 <hwanhuicho916@gmail.com>
2024-11-28 11:20:19 +0900

    k2

--- code/main.py
+++ code/main.py
@@ -7,6 +7,7 @@ from groups import AllSprites
 class Game:
     def __init__(self):
         #settings
+
         pygame.init()
         self.display_surface  = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption('Survivor')
