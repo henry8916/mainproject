5016b63d817ca93a0ad6eee4877ea3254fa48025
yul080708 <yul080708@gmail.com>
2024-12-02 17:59:06 +0900

    Add files via upload

---
+++ holesmap/Tiletsx/sand.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Sand" tilewidth="87" tileheight="64" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../Tilesets/sand.png" width="87" height="64"/>
+ </tile>
+</tileset>
