9e27581901b191e2982bf0e60c3f746ac3fc60fa
dohyung0821@gmail.com <ko992619@@>
2024-12-04 04:33:49 +0900

    dialog

--- code/settings.py
+++ code/settings.py
@@ -4,7 +4,7 @@ from os import walk
 
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280,720
 TILE_SIZE = 64
-
+ANIMATION_SPEED=6
 def doublingimage(image):
     original_width, original_height = image.get_size()
     scaled_image = pygame.transform.scale(image, (original_width * 2, original_height * 2))
